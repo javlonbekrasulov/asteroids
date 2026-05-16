43b254712b2e1c7ff1beeb547a7706cd2836f8c2
javlonbekrasulov <javlonbekrasulovv@gmail.com>
2026-05-16 19:00:17 +0900

    black screen set up

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from logger import log_state
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            pass
+        screen.fill("black")
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+            return
+
+
+
+
+
+if __name__ == "__main__":
+    main()
