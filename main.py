f6b0cc8ff19dacd39a3f2aefb6825d37aa4b477b
javlonbekrasulov <javlonbekrasulovv@gmail.com>
2026-05-19 21:10:58 +0900

    drawing circle in the game

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import pygame
+from player import Player
 from logger import log_state
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    klok = pygame.time.Clock()
+    dt = 0
+    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
     while True:
-        log_state()
-        for event in pygame.event.get():
-            pass
-        screen.fill("black")
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-            return
+                return
+            else:
+                pass
+        log_state()
+        
+        screen.fill("black")
+        player.draw(screen)
 
-
-
-
+        pygame.display.flip()
+        tiking = klok.tick(60)
+        dt = tiking / 1000
 
 if __name__ == "__main__":
     main()
